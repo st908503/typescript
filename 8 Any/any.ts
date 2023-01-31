3934617ab4ac403aa2919dac1f9542c6488b88eb
Shashank Tripathi <60851994+st908503@users.noreply.github.com>
2023-01-31 19:54:59 +0530

    Add files via upload

---
+++ 8 Any/any.ts
@@ -0,0 +1,12 @@
+//when we are not sure which type of data has to be held by an array , we use Any datatype
+
+
+let data: any[] = [3, 5, 'hello', true]
+
+
+// it also has a disadvantage
+
+// sometimes we know that a variable is only going to contain either string or only number or  only boolean in that case also if we use any[] instead or or | then there are more chances of getting errors.
+
+// so we should avoid using any[] most of the times
+
