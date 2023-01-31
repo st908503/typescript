8b2dfd9ea35d42b74f735efb7e707549d8522e7c
Shashank Tripathi <60851994+st908503@users.noreply.github.com>
2023-01-31 19:53:56 +0530

    Add files via upload

---
+++ 4 objectype/object.js
@@ -0,0 +1,15 @@
+var person = {
+    name: 'Shashank',
+    age: 23,
+    email: 'sda@sfdf.com'
+};
+console.log(person.name);
+var user = {
+    name: 'Shashank',
+    age: 23,
+    email: 'sda@sfdf.com'
+};
+console.log(user.name);
+//interview quesion:
+// what is object type and how can you make it?
+// showed above
---
+++ 4 objectype/object.ts
@@ -0,0 +1,29 @@
+type objtype = {name:string, age:number, email:string}
+
+const person:objtype={
+    name: 'Shashank',
+    age: 23,
+    email: 'sda@sfdf.com'
+};
+console.log(person.name)
+
+// if we want multiple object with same fields as name age and email we can define it at the top and use as data type with our object variables
+
+const user:objtype ={
+    name: 'Shashank',
+    age: 23,
+    email: 'sda@sfdf.com'
+};
+console.log(user.name)
+
+
+
+
+
+
+
+
+//interview quesion:
+
+// what is object type and how can you make it?
+// showed above
