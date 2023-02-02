6fb0e282af5c3ae724b70948e153faa2ad4bf71a
Shashank Tripathi <60851994+st908503@users.noreply.github.com>
2023-02-02 23:54:48 +0530

    Add files via upload

---
+++ 24 constructor/app.js
@@ -0,0 +1,39 @@
+"use strict";
+// class Users {
+//     name: '';
+//     age: '';
+//     email : '';
+// constructor(name, age, email)
+// {
+//    this.name = name;
+//    this.age = age;
+//    this.email =email;
+// }
+// displayVal(){
+//     console.log(this.name, this. age, this. email)
+// }
+// }
+// // constructor gets called automatically whenever object is created
+// const u1 = new Users('ashank', 34, 'dsfsd@xx.com');
+// u1.displayVal();
+// now for the same if we removev below lines from and put public then also our code will work
+// name: '';
+//     age: '';
+//     email : '';
+// and 
+// this.name = name;
+// this.age = age;
+// this.email =email;
+class Users {
+    constructor(name, age, email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+    displayVal() {
+        console.log(this.name, this.age, this.email);
+    }
+}
+// constructor gets called automatically whenever object is created
+const u1 = new Users('ashank', 34, 'dsfsd@xx.com');
+u1.displayVal();
